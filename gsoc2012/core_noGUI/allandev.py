Corrected Allan Calculation, implemented Residuals plots from Least Square Line Subtracting, enabled PPM plot for Allan Deviation

gsoc2012/core_noGUI/allandev.py
@@ -18,6 +18,7 @@ __status__ = "Prototype"
 from scipy import *
 import scipy
 from math import *
+import numpy as np
 
 
 class AllanDev():
@@ -27,6 +28,39 @@ class AllanDev():
         self.av = [0]
         self.timeS = [0]
         self.error = [0]
+
+    def allanDevMills(self, values):
+
+    ##########
+    # Allan Deviation Calculation Routine adapted from David Mills Matlab sub-routine
+    ##########
+
+        timeInterval = []
+        aDev = []
+        error = []
+        N = len(values)
+
+        y1 = np.asarray(values)
+        d=10
+        i=0
+        while(len(y1) >=10 ):
+            u = diff(y1)/d
+            v = diff(u)
+            temp = sqrt(mean(v * v)/2)
+            aDev.append(temp)
+            timeInterval.append(d)
+            y1 = y1[::2]
+            temp2 = 1/sqrt(2*(((float)(N)/(pow(2,i)))-1))
+            temp2 *= aDev[i]
+            error.append(temp2)
+            i=i+1
+            d=d*2
+
+
+        return timeInterval,aDev,error
+
+
+
 
     def allanDev(self,values, tau0):
 
@@ -57,7 +91,7 @@ class AllanDev():
 
 
 
-                val = values[(int)(l):(int)((l+((pow(2,i))-1)))]
+                val = np.asarray(values[(int)(l):(int)((l+((pow(2,i))-1)))])
                 tempSum = scipy.sum(val)
 
                 omega[k] = (float)(tempSum)/(pow(2,i))
@@ -66,7 +100,7 @@ class AllanDev():
 
             sumvalue = 0
 
-            #Perfome the difference of the average values
+            #Perfom the difference of the average values
             for k in range(1,(len(omega)-1)):
 
                 sumvalue += pow((omega[k+1]-omega[k]),2)
@@ -83,6 +117,7 @@ class AllanDev():
             self.error[i+1] *= self.av[i+1]
 
         return self.timeS, self.av,self.error
+
 
     def allanDevn(self,values, tau0):
 

gsoc2012/core_noGUI/loopLoader.py
@@ -19,6 +19,8 @@ from scipy import *
 import pylab
 import allandev
 
+import numpy as np
+
 class loopLoader():
 
     def __init__(self):
@@ -29,6 +31,7 @@ class loopLoader():
         self.timeS = []
         self.av = []
         self.error = []
+        self.day = []
 
         self.file = None
         self.file2 = None
@@ -39,9 +42,14 @@ class loopLoader():
 
         readLines = self.file.readlines()
 
+        self.offsets = []
+        self.seconds = []
+        self.day = []
+
         for line in readLines:
             line = line.strip()
             line=line.split()
+            self.day.append((float)(line[0]))
             self.offsets.append((float)(line[2]))
             self.seconds.append((float)(line[1]))
 
@@ -97,6 +105,18 @@ class loopLoader():
 
         pylab.show()
 
+        #PPM PLOT
+
+       # pylab.loglog(self.timeS, np.asarray(self.av)*1e6, 'b^', self.timeS, np.asarray(self.av) *1e6)
+       # pylab.errorbar(self.timeS, np.asarray(self.av)*1e6,yerr=np.asarray(self.error),fmt='k.' )
+
+        #pylab.legend(('ADEV points', 'ADEV'))
+
+
+        #pylab.grid(True)
+
+        #pylab.show()
+
 
 
 
@@ -106,5 +126,6 @@ if __name__ == '__main__':
     allantest = loopLoader()
     allantest.processLoopStats('loopstats.txt')
     allantest.savetoFile("offsets.txt")
-    [allantest.timeS, allantest.av, allantest.error] = allantest.Allan.allanDev(allantest.offsets, 10)
+    #[allantest.timeS, allantest.av, allantest.error] = allantest.Allan.allanDev(allantest.offsets, 10)
+    [allantest.timeS, allantest.av, allantest.error] = allantest.Allan.allanDevMills(allantest.offsets)
     allantest.plotnoGui()

gsoc2012/gui/plotSpace.py
@@ -19,6 +19,8 @@ from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from time import ctime
 
+import numpy as np
+
 from math import *
 
 
@@ -29,6 +31,67 @@ class plotSpace(FigureCanvas):
 
         self.subplot = self.figure.add_subplot(111)
         self.subplot.hold(True)
+
+    def plotMinusLeastSquares(self,type, off, sec, av, error, timeS, tickCorrect):
+
+        self.subplot.clear()
+
+        self.subplot.set_xlabel('Hours(h)')
+        self.subplot.set_ylabel('Residuals(ms)')
+
+        a,b = np.polyfit(sec,off,1)
+        secArray = np.asarray(sec)
+        offArray = np.asarray(off)
+
+        self.subplot.plot(secArray, offArray-(a*secArray+b), '--k')
+
+        xMin = sec[0]
+        xMax = sec[len(sec)-1]
+
+        xMin = (int)((floor)(xMin))
+
+        xMax = (int)((floor)(xMax))
+
+        self.subplot.set_xlim([xMin,xMax])
+
+        toPlot = []
+        toPlotTime = []
+
+        for i in range(len(sec)):
+            if i%4 == 0:
+                ct = ctime(sec[i])
+                ct = ct.split(' ')
+                if '' in ct:
+                    ct.remove('')
+                ct = ct[3].split(':')
+                toPlot.append(ct[0])
+                toPlotTime.append(sec[i])
+
+
+
+        self.subplot.set_title("Offsets Subtracted by a Least Squares Fit")
+
+        self.subplot.grid()
+
+        if tickCorrect:
+            self.subplot.set_xticks(toPlotTime)
+            self.subplot.set_xticklabels(toPlot)
+
+        self.draw()
+
+    def plotAllanPPM(self, type, off, sec, av, error, timeS, tickCorrect):
+
+        self.subplot.clear()
+
+        self.subplot.set_xlabel(r'$\tau$ - sec')
+        self.subplot.set_ylabel(r'$\sigma(\tau$) - PPM')
+        self.subplot.set_title('Allan Standard Deviation')
+        plot2 = self.subplot.loglog(timeS, np.asarray(av)*1e6, 'b^', timeS, np.asarray(av) *1e6)
+        plot1 = self.subplot.errorbar(timeS, np.asarray(av)*1e6,yerr=np.asarray(error)*1e6,fmt='k.' )
+        self.subplot.grid()
+        self.subplot.legend(('ADEV points', 'ADEV'))
+
+        self.draw()
 
     def update_plot(self, type, off, sec, av, error, timeS, tickCorrect):
 
@@ -77,8 +140,8 @@ class plotSpace(FigureCanvas):
         elif(type==2):
 
 
-            self.subplot.set_xlabel(r'$\tau$')
-            self.subplot.set_ylabel(r'$\sigma(\tau$)')
+            self.subplot.set_xlabel(r'$\tau$ - sec')
+            self.subplot.set_ylabel(r'$\sigma(\tau$) - ms')
             self.subplot.set_title('Allan Standard Deviation')
             plot2 = self.subplot.loglog(timeS, av, 'b^', timeS, av)
             plot1 = self.subplot.errorbar(timeS, av,yerr=error,fmt='k.', animated=True )
@@ -89,8 +152,12 @@ class plotSpace(FigureCanvas):
 
         elif(type==3):
 
-            self.subplot.set_title("Histogram of the Offsets")
-            self.subplot.hist(off)
+            self.subplot.set_title("Normalized Histogram of the Offsets")
+            self.subplot.hist(off, 50, normed=True, facecolor='green', alpha=0.75)
+            self.subplot.set_xlabel('Offsets')
+            self.subplot.set_ylabel('Probability')
+            self.subplot.set_title(r'$\mathrm{Histogram\ of\ the\ Offsets}$')
+
             self.subplot.grid(True)
             self.draw()
 